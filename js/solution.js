b3a1995a489485691e69548d0e3fa7f6e384a7aa
Srjkaa <srjkaa@yandex.by>
2019-09-19 13:55:15 +0300

    Added description for 'notInChecked' function, changed postfix increment to postfix increment, changed indents

--- js/solution.js
+++ js/solution.js
@@ -10,15 +10,17 @@
      * @returns {number} кол-во островов
      */
     let point = 0;
-    const shift = 1;
     let checked = [];
+    const shift = 1;
 
     function solution(map) {
-        // todo: подсчитать кол-во островов на карте
         let islands = 0;
         let checkedIndex = 0;
-        for (let r = 0; r < map.length; r++) {
-            for (let c = 0; c < map[r].length; c++) {
+        const rowsLength = map.length;
+        const colsLength = map[0].length;
+
+        for (let r = 0; r < rowsLength; ++r) {
+            for (let c = 0; c < colsLength; ++c) {
                 if (map[r][c] === 1 && notInChecked(r, c)) {
                     point = [r,c];
                     checked.push(point);
@@ -27,64 +29,77 @@
                         verticalShift(map, checked[checkedIndex][0], checked[checkedIndex][1]);
                         checkedIndex++;
                     }
-                islands++;
+                ++islands;
                 }
             }
         }
         return islands;
-    };
+    }
 
     function horizontalShift(map, x, y) {
-    let positiveShift = true;
-    let negativeShift = true;
-    for (let i = x, j = y; ((j < map[0].length) && (positiveShift)); j++) {
-        if (map[i][j + shift] === 1 && notInChecked(i,j + shift)) {
-            point = [i,j + shift];
-            checked.push(point);
-        } else {
-            positiveShift = false;
+        let positiveShift = true;
+        let negativeShift = true;
+        const length = map[0].length;
+
+        for (let i = x, j = y; ((j < length) && (positiveShift)); ++j) {
+            if (map[i][j + shift] === 1 && notInChecked(i,j + shift)) {
+                point = [i,j + shift];
+                checked.push(point);
+            } else {
+                positiveShift = false;
+            }
+        }
+
+        for (let i = x, j = y; ((j >= 0) && (negativeShift)); --j) {
+            if (map[i][j - shift] === 1 && notInChecked(i,j - shift)) {
+                point = [i,j - shift];
+                checked.push(point);
+            } else {
+                negativeShift = false;
+            }
         }
     }
-    for (let i = x, j = y; ((j >= 0) && (negativeShift)); j--) {
-        if (map[i][j - shift] === 1 && notInChecked(i,j - shift)) {
-            point = [i,j - shift];
-            checked.push(point);
-        } else {
-            negativeShift = false;
-        }
-    }
-};
 
     function verticalShift(map, x, y) {
-    let positiveShift = true;
-    let negativeShift = true;
-    for (let i = x, j = y; ((i < map.length) && (i != map.length - 1) && (positiveShift)); i++) {
-        if (map[i + shift][j] === 1 && notInChecked(i + shift, j)) {
-            point = [i + shift,j];
-            checked.push(point);
-        } else {
-            positiveShift = false;
-        }
-    }
-    for (let i = x, j = y; ((i > 0) && (negativeShift)); i--) {
-        if (map[i - shift][j] === 1 && notInChecked(i - shift, j)) {
-            point = [i - shift,j];
-            checked.push(point);
-        } else {
-            negativeShift = false;
-        }
-    }
-};
+        let positiveShift = true;
+        let negativeShift = true;
+        const length = map.length;
 
-    // принимает координаты х, у текущей точки на проверку того, встречалась ли она ранее 
+        for (let i = x, j = y; ((i < length) && (i !== length - 1) && (positiveShift)); ++i) {
+            if (map[i + shift][j] === 1 && notInChecked(i + shift, j)) {
+                point = [i + shift,j];
+                checked.push(point);
+            } else {
+                positiveShift = false;
+            }
+        }
+
+        for (let i = x, j = y; ((i > 0) && (negativeShift)); --i) {
+            if (map[i - shift][j] === 1 && notInChecked(i - shift, j)) {
+                point = [i - shift,j];
+                checked.push(point);
+            } else {
+                negativeShift = false;
+            }
+        }
+    }
+
+    /**
+     * @desc Take x and y coordinates of current point for check is point already checked
+     * @param x { number } x value of current point
+     * @param y { number } y value of current point
+     * @return { boolean }
+     */
     function notInChecked(x, y) {
-        for (let i = 0; i < checked.length; i++) {
+        const length = checked.length;
+
+        for (let i = 0; i < length; ++i) {
             if (checked[i][0] === x && checked[i][1] === y) {
                 return false;
             }
         }
         return true;    
-    };
+    }
 
     root.SHRI_ISLANDS.solution = solution;
 })(this);
